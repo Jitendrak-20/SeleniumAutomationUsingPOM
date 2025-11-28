4a38b91811605c9eb3c50da86d449700ce1e1fa5
jiten <jiten@LAPTOP-972JNURF>
2025-11-28 17:15:16 +0530

    Base page and Base test added

--- src/main/java/com/testing/base/BasePage.java
+++ src/main/java/com/testing/base/BasePage.java
@@ -1,11 +1,49 @@
 package com.testing.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BasePage {
 	
+	protected WebDriver driver;
 	
+	// Constructor
 	
+	public WebDriver getDriver(String browser) {
+		if(browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		}else if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}else {
+			driver = new EdgeDriver();
+		}
+		return driver;
+	}
 	
+	public void quitDriver() {
+		driver.quit();
+	}
+	
+	//Common methods 
+	
+	//Logo
+	public boolean verifyLogo() {
+		return false;
+	}
+	
+	//Verify Title
+	
+	public String getTitle() {
+		return driver.getTitle();
+	}
+	
+	//CurrentURL
+	public String getCurrentUrl() {
+		return driver.getCurrentUrl();
+	}
 
 }
--- src/test/java/com/testing/basetest/BaseTest.java
+++ src/test/java/com/testing/basetest/BaseTest.java
@@ -1,5 +1,37 @@
 package com.testing.basetest;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import com.testing.base.BasePage;
+import com.testing.pages.RegistrationPage;
+import com.testing.pages.SignInPage;
+
 public class BaseTest {
+	
+	public BasePage basePage;
+	public WebDriver driver;
+	public RegistrationPage registrationPage;
+	public SignInPage signInPage;
+	@BeforeMethod
+	public void baseSetup() {
+		
+		basePage = new BasePage();
+		basePage.getDriver("chrome");
+		driver.get("url of the App");
+		
+		
+		registrationPage = new RegistrationPage();
+		signInPage = new SignInPage();
+		
+		
+	}
+	@AfterMethod
+	
+	public void closeBrowser() {
+		basePage.quitDriver();
+	}
+	
 
 }
